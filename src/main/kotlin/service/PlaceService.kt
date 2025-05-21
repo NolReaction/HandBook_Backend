70b8193459bc7012f7f6cb58d1e34c7cca011212
Dmitry <slomov.dima@mail.ru>
2025-05-21 22:04:31 +0300

    V0.9.3 -Увеличино количество тестируемых мест

--- src/main/kotlin/service/PlaceService.kt
+++ src/main/kotlin/service/PlaceService.kt
@@ -15,15 +15,55 @@ class PlaceService(private val connection: Connection) {
             );
         """
 
-        // Пара тестовых записей
+        // Тестовые записи
         private const val SEED_DATA = """
-          INSERT INTO places (name, rating, description)
-          SELECT 'Кафе "Уют"', 4.2, 'Уютное место с отличным кофе'
-          WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Кафе "Уют"');
-          
-          INSERT INTO places (name, rating, description)
-          SELECT 'Парк "Солнечный"', 3.8, 'Зелёный парк для прогулок'
-          WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Парк "Солнечный"');
+            INSERT INTO places (name, rating, description)
+            SELECT 'Кафе "Уют"', 4.2, 'Уютное место с отличным кофе'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Кафе "Уют"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Парк "Солнечный"', 3.8, 'Зелёный парк для прогулок'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Парк "Солнечный"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Ресторан "Лакомка"', 4.7, 'Гастрономический ресторан итальянской кухни'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Ресторан "Лакомка"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Музей современного искусства"', 4.4, 'Экспозиции российских и зарубежных художников'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Музей современного искусства"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Книжная лавка "Читай-город"', 4.1, 'Уютная атмосфера и редкие издания'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Книжная лавка "Читай-город"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Кинотеатр "Оскар"', 4.5, 'Премьеры мирового кино в формате 4K'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Кинотеатр "Оскар"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Аквариум "Глубины"', 4.3, 'Морские обитатели со всего мира'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Аквариум "Глубины"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Зоопарк "Лесная сказка"', 4.0, 'Редкие виды животных и интерактивные программы'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Зоопарк "Лесная сказка"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Библиотека "Знание"', 4.6, 'Современные залы для чтения и работы'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Библиотека "Знание"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Коворкинг "Планета"', 4.2, 'Комфортные рабочие места и переговорные комнаты'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Коворкинг "Планета"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Фитнес-центр "Атлет"', 4.4, 'Современные тренажёры и групповые занятия'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Фитнес-центр "Атлет"');
+        
+            INSERT INTO places (name, rating, description)
+            SELECT 'Пляж "Золотой берег"', 4.8, 'Белый песок и чистая вода'
+            WHERE NOT EXISTS (SELECT 1 FROM places WHERE name = 'Пляж "Золотой берег"');
         """
     }
 
